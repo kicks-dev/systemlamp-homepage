86954b25026f4edf3b569fe0fdd4fdb60638e8e0
MasahikoKondo <mkondo.lamp@gmail.com>
2018-02-10 00:40:19 +0900

    リクルートページ作成

--- src/script/index.js
+++ src/script/index.js
@@ -108,7 +108,7 @@ function initKyusyuMap() {
 }
 $(function(){
 
-  $('.effect div, .effect i').css("opacity","0");
+  
   $('.show-card-from-top').css("opacity","0");
   $('.show-card-from-left').css("opacity","0");
   $('.show-card-from-right').css("opacity","0");
@@ -116,39 +116,41 @@ $(function(){
   var timer = null;
   $(window).scroll(function (){
     clearTimeout(timer);
-    console.log("scroll");
     timer = setTimeout(function(){
-
-      effectScroll();
       showCardTopScroll();
       showCardLeftScroll();
       showCardRightScroll();
       showTextScroll();
-    }, 300);
+    }, 200);
   });
 })
 
 // 徐々に現れるテキスト
-var effectScroll = function(){
-  $(".effect").each(function(){
-    var imgPos = $(this).offset().top;    
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll > imgPos - windowHeight + windowHeight/5){
-      $("i, div",this).css("opacity","1" );
-      $("i",this).css({ 
-        "font-size": "100px",
-        "padding": "0 20px 40px"
-      });
-    } else {
-      $("i, div",this).css("opacity","0" );
-      $("i",this).css({ 
-        "font-size": "20px",
-        "padding": "20px"
-      });
-    }
+$(function(){
+  $('.effect div, .effect i').css("opacity","0");
+  $(window).scroll(function (){
+    $(".effect").each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $("i, div",this).css("opacity","1" );
+        $("i",this).css({ 
+          "font-size": "100px",
+          "padding": "0 20px 40px"
+        });
+      } else {
+        $("i, div",this).css("opacity","0" );
+        $("i",this).css({ 
+          "font-size": "20px",
+          "padding": "20px"
+        });
+      }
+    });
   });
-}
+});
+  
+
 
 // 徐々に下から現れるカード
 var showCardTopScroll = function(){
